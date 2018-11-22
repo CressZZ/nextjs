b775d18af8a807c440e04c1b3cac35df9c2d8f7a
CressZZ <dktsjssnrnsi@gmail.com>
2018-11-22 21:38:17 +0900

    init

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,18 @@
-import Link from 'next/link'
+import React, { Component } from 'react';
+import Link from 'next/link';
 
-/*
-export default () => 
-     <div>Hello, Next.js?!</div>
-*/
-
-// 모듈 import 하는 것도 없는건 당연한가?
-// 함수형 이기 때문입니다. 
-const Test = ()=>{
-    return (
-        <div>
-            <p>Hello, Next.js !?</p>
+class Index extends Component {
+    render() {
+        return (
             <div>
-                <Link href="/playground">
-                    <a>Go Playground</a>
+                <h1>Bootstrap Demo</h1>
+
+                <Link href='/images'>
+                    <a className="btn btn-primary">Profile Images</a>
                 </Link>
             </div>
-            <div>
-                <a href="/playground">Go Playground (Without Link)</a>
-            </div>
-        </div>
-    )
+        );
+    }
 }
 
-export default Test;
+export default Index;
